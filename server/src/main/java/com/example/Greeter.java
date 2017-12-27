54058a4fc781526bfe4f5b5bd2828b9d46bc8f91
thakkarjanak <thakkarjanak1811@gmail.com>
2017-12-27 20:52:59 +0530

    Create Package Info Java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
    /**
-    * Test JAVA Comment for greet method
+    * @param someone the name of person
+    * @return greeting string
     */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
