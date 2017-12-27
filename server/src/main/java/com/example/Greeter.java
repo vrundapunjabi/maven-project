9c71aa0486515e43d78e8ce05a160cf1bfd4ff28
thakkarjanak <thakkarjanak1811@gmail.com>
2017-12-27 20:13:40 +0530

    Fixed Code Quality Errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/**
+* Test JAVA Comment for greet method
+ */
+
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
